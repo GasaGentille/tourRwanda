312135d4a29a95270bb0f53f4cdbe66453def44a
Benedicte-Manishimwe <benoite251@gmail.com>
2019-08-08 21:44:50 +0200

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,21 @@
-function kigaliFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+// function kigaliFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  // event.preventDefault();
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-function northFunction(){
-  document.getElementById("northDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-north");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   // event.preventDefault();
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
 // function northFunction(){
 //   document.getElementById("northDropdown").classList.toggle("show");
 // }
@@ -50,3 +33,25 @@ window.onclick = function(event) {
 //     }
 //   }
 // }
+// // function northFunction(){
+// //   document.getElementById("northDropdown").classList.toggle("show");
+// // }
+
+// // // Close the dropdown if the user clicks outside of it
+// // window.onclick = function(event) {
+// //   if (!event.target.matches('.dropbtn')) {
+// //     var dropdowns = document.getElementsByClassName("dropdown-north");
+// //     var i;
+// //     for (i = 0; i < dropdowns.length; i++) {
+// //       var openDropdown = dropdowns[i];
+// //       if (openDropdown.classList.contains('show')) {
+// //         openDropdown.classList.remove('show');
+// //       }
+// //     }
+// //   }
+// // }
+$("#book").click(function() {
+  alert("Thanks for the book");
+});
+// ktdjhtdfhkdfhkchkuio
+// ;khugjvilb
